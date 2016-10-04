c6feb3b53a411d73ba0a5457cc2d0410313d8f92
Tamanh BUI <tbui@e2r8p8.42.fr>
2016-10-04 14:10:18 +0200

    Small changes

--- instruction.c
+++ instruction.c
@@ -11,3 +11,38 @@
 /* ************************************************************************** */
 
 
+void		apply_add(t_data *data)
+{
+	int store;
+	int reg_one;
+	int reg_two;
+
+	reg_one = data.map[data->begin.pc + 2 % MEM_SIZE];
+	reg_two = data.map[data->begin.pc + 3 % MEM_SIZE];
+	store = data.map[data->begin.pc + 4 % MEM_SIZE];
+	reg_number[store] = reg_number[reg_one] + reg_number[reg_two];
+	data->begin.carry = 1;
+}
+
+void		apply_sub(t_data *data)
+{
+	int store;
+	int reg_one;
+	int reg_two;
+
+	reg_one = (int)data.map[data->begin.pc + 2 % MEM_SIZE];
+	reg_two = (int)data.map[data->begin.pc + 3 % MEM_SIZE];
+	store = (int)data.map[data->begin.pc + 4 % MEM_SIZE];
+	reg_number[store] = reg_number[reg_one] - reg_number[reg_two];
+	data->begin.carry = 1;
+}//same as add but subtraction
+//add & sub functions above
+
+
+//deteermining the paramater types below
+void		det_(int types)
+{
+	
+
+}
+//determining the parameter types above
