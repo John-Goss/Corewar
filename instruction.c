a0a1db32b1d102134bf03c4ffce8f51c7b242e28
Tamanh BUI <tbui@e1r10p8.42.fr>
2016-10-10 18:18:03 +0200

    instructions are finished, except for fork, lfork, and aff

--- instruction.c
+++ instruction.c
@@ -12,6 +12,11 @@
 
 #include "corewar.h"
 
+//Finish coding all the single instructions
+//learn how to get a direct parameter out of either 4 or 2 bytes, presumably with bit operators
+//find out how to direct the process into the given instruction
+//test and refactor EVERYTHING!
+
 void        instr_no_ocp(t_data *data, t_list *elem)
 {
     if (pc == 0x0C)  
@@ -58,12 +63,12 @@ void		apply_add(t_data *data, t_list *elem)
 	int reg_one;
 	int reg_two;
 
-	reg_one = data->map[(elem->pc + (2 % IDX_MOD)) % MEM_SIZE];
-	reg_two = data->map[(elem->pc + (3 % IDX_MOD)) % MEM_SIZE];
-	store = data->map[(elem->pc + (4 % IDX_MOD)) % MEM_SIZE];
+	reg_one = data->map[(elem->pc + 2) % MEM_SIZE];
+	reg_two = data->map[(elem->pc + 3) % MEM_SIZE];
+	store = data->map[(elem->pc + 4) % MEM_SIZE];
 	elem->reg_number[store] = elem->reg_number[reg_one] + elem->reg_number[reg_two];
 	elem->carry = 1;
-	elem->pc = ((elem->pc + (4 % IDX_MOD)) % MEM_SIZE);
+	//elem->pc = ((elem->pc + (4 % IDX_MOD)) % MEM_SIZE);
 }
 
 void		apply_sub(t_data *data, t_list *elem)
@@ -71,12 +76,12 @@ void		apply_sub(t_data *data, t_list *elem)
 	int store;
 	int reg_one;
 	int reg_two;
-    reg_one = data->map[(elem->pc + (2 % IDX_MOD)) % MEM_SIZE];
-    reg_two = data->map[(elem->pc + (3 % IDX_MOD)) % MEM_SIZE];
-    store = data->map[(elem->pc + (4 % IDX_MOD)) % MEM_SIZE];
+    reg_one = data->map[(elem->pc + 2) % MEM_SIZE];
+    reg_two = data->map[(elem->pc + 3) % MEM_SIZE];
+    store = data->map[(elem->pc + 4) % MEM_SIZE];
 	elem->reg_number[store] = elem->reg_number[reg_one] - elem->reg_number[reg_two];
 	elem->carry = 1;
-	elem->pc = (elem->pc + 4) % MEM_SIZE;
+	//elem->pc = (elem->pc + 4) % MEM_SIZE;
 }//same as add but subtraction
 //add & sub functions above
 
@@ -99,52 +104,24 @@ void		apply_live(t_data *data, t_list *elem)
 void		apply_ld(t_data *data, t_list *elem, int *params, int *param_types)
 {
     //value of first parameter into register
-    if (param_type[0] == DIR_CODE || param_type[0] == IND_CODE)
-        elem->reg_number[params[1]] = data->map[params[0]];// param[0] is the first parameter
+    if (param_type[0] == DIR_CODE)
+        elem->reg_number[params[1]] = params[0];// param[0] is the first parameter
+    else if (param_type[0] == DIR_CODE)
+        elem->reg_number[params[1]] = get_ind_value_idxd(params[0]);
     else if (param_types[0] == REG_CODE)
-    elem->reg_number[params[1]] = elem->reg_number[params[0]];
+        elem->reg_number[params[1]] = elem->reg_number[params[0]];
     //params[1] is the vnumber of the register we need
 }
 
 void		apply_st(t_data *data, t_list *elem, int *params, int *param_types)
 {
-<<<<<<< HEAD
-	char *bin_nbr;
-	int remainder;
-	int i;
-	int k;
 
-	i = 1;
-	k = 0;
-	bin_nbr = ft_strnew(9);
-	while (n != 0)
-	{
-		remainder = n % 2;
-		n /= 2;
-
-		if (remainder == 0)
-			bin_nbr[k] = '0';
-		else if (remainder == 1)
-			bin_nbr[k] = '1';
-		i *= 10;
-		k++;
-	}
-	while (k < 8)
-	{
-		bin_nbr[k] = '0';
-		k++;
-	}
-	return (bin_nbr);
-}
-=======
     if (param_types[1] == IND_CODE)//go to the address (PC plus value) and store first value
         data->map[params[1]] =  elem->reg_number[params[0]]; //value to copy into the other place
     else if (param_types[1] == REG_CODE)//put the value to be copied into the register
         elem->reg_number[params[1]] = elem->reg_number[params[0]]; //value to copy into the other place
 }//this takes the value of a register and STORES it at either an address or another register
 
-
->>>>>>> d4efa2824cd752645eb8384482a4dec36e4ce2bb
 
 //takes two paramters of which the third will always be register
 void		apply_and(t_data *data,t_list *elem, int *param, int *param_types)
@@ -154,14 +131,17 @@ void		apply_and(t_data *data,t_list *elem, int *param, int *param_types)
 
     if (params_type[0] == REG_CODE)
         value_one = elem->reg_number[params[0]];
-    else if (params_type[0] == IND_CODE || params_type[0] == DIR_CODE)
-        value_one = data->map[params[0]];
+    else if (params_type[0] == DIR_CODE)
+        value_one = params[0];
+    else if (params_type[0] == IND_CODE)
+        value_one = get_ind_value_idxd(params[0]);
     if (params_type[1] == REG_CODE)
         value_two = elem->reg_number[params[1]];
-    else if (params_type[1] == IND_CODE || params_type[1] == DIR_CODE)
-        value_two = data->map[params[1]];
-    //so now we got the two values which we need to stock
-    elem->reg_number[params[2]] = value_one & value_two;
+    else if (params_type[1] == DIR_CODE)
+        value_two = params[1];
+    else if (params_type[1] == IND_CODE)
+        value_one = get_ind_value_idxd(params[0]);
+    elem->reg_number[params[2]] = value_one & value_two;//so now we got the two values which we need to stock
 }//the bit operation & is executed on the first two and then stored at the third which is a register
 
 void		apply_or(t_data *data, t_list *elem)
@@ -171,13 +151,16 @@ void		apply_or(t_data *data, t_list *elem)
 
     if (params_type[0] == REG_CODE)
         value_one = elem->reg_number[params[0]];
-    else if (params_type[0] == IND_CODE || params_type[0] == DIR_CODE)
-        value_one = data->map[params[0]];
+    else if (params_type[0] == DIR_CODE)
+        value_one = params[0];
+    else if (params_type[0] == IND_CODE)
+        value_one = get_ind_value_idxd(params[0]);
     if (params_type[1] == REG_CODE)
         value_two = elem->reg_number[params[1]];
-    else if (params_type[1] == IND_CODE || params_type[1] == DIR_CODE)
-        value_two = data->map[params[1]];
-    //so now we got the two values which we need to stock
+    else if (params_type[1] == DIR_CODE)
+        value_two = params[1];
+    else if (params_type[1] == IND_CODE)
+        value_one = get_ind_value_idxd(params[0]);
     elem->reg_number[params[2]] = value_one | value_two;
 }
 
@@ -188,62 +171,120 @@ void		apply_xor(t_data *data, t_list *elem)
 
     if (params_type[0] == REG_CODE)
         value_one = elem->reg_number[params[0]];
-    else if (params_type[0] == IND_CODE || params_type[0] == DIR_CODE)
-        value_one = data->map[params[0]];
+    else if (params_type[0] == DIR_CODE)
+        value_one = params[0];
+    else if (params_type[0] == IND_CODE)
+        value_one = get_ind_value_idxd(params[0]);
     if (params_type[1] == REG_CODE)
         value_two = elem->reg_number[params[1]];
-    else if (params_type[1] == IND_CODE || params_type[1] == DIR_CODE)
-        value_two = data->map[params[1]];
-    //so now we got the two values which we need to stock
+    else if (params_type[1] == DIR_CODE)
+        value_two = params[1];
+    else if (params_type[1] == IND_CODE)
+        value_one = get_ind_value_idxd(params[0]);
     elem->reg_number[params[2]] = value_one ^ value_two;
 
 
 }
 
-void		apply_zjmp(t_data *data, t_list *elem, int indx)
+void		apply_zjmp(t_data *data, t_list *elem, int *params)
 {
     if (elem->carry == 1)
-            elem->pc = (elem->pc + (indx % IDX_MOD)) % MEM_SIZE;
+            elem->pc = (elem->pc + (params[0] % IDX_MOD)) % MEM_SIZE;
 }
 
-//void		apply_ldi(t_data *data, t_list *elem)
-//{
-//
-//}
-//
-//void		apply_sti(t_data *data, t_list *elem)
-//{
-//
-//}
-//
-                  
+void		apply_ldi(t_data *data, t_list *elem, int *param_types, int *params)
+{
+    int value_one;
+    int value_two;
+    int store; //value to store into the given register
 
-//void		apply_fork(t_data *data, t_list *elem)
-//{
-//
-//}
-//
+    if (param_types[0] == DIR_CODE)
+        value_one = params[0];
+    else if (param_types[0] == IND_CODE)
+        value_one = get_ind_value_idxd(params[0]);
+    else if (param_types[0] == REG_CODE)
+        value_one = elem->reg_number[params[0]];
+    if (param_types[1] == DIR_CODE)
+        value_two = params[1]
+    else if (param_types[1] == REG_CODE)
+        value_two = elem->reg_number[params[1]];
+    store = get_ind_value_idxd(value_one + value_two);
+    elem->reg_number[params[2]] = store;
 
-//void		apply_lld(t_data *data, t_list *elem) //addresses not relative to IDX
-//{
-//
-//}
-//
-//void		apply_lldi(t_data *data, t_list *elem) //addresses not relative to IDX
-//{
-//
-//}
-//
+}//{"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25,
+//        "load index", 1, 1},
+
+void		apply_sti(t_data *data, t_list *elem, int *param_types, int *params)
+{
+    int value_one;
+    int value_two;
+
+    if (param_types[1] == DIR_CODE)
+        value_one = params[1];
+    else if (param_types[1] == IND_CODE)
+        value_one = get_ind_value_idxd(params[1]);
+    else if (param_types[0] == REG_CODE)
+        value_one = elem->reg_number[params[1]]; //determining the first value which we will use as the an index
+    if (param_types[2] == DIR_CODE)
+        value_two = params[2];
+    else if (param_types[2] == REG_CODE)
+        value_two = elem->reg_number[params[2]];
+    elem->reg_number[params[0]] = get_ind_value_idxd(value_one + value_two); //getting the value at the address which we've determined by combining the values of the last two parameters
+        //then putting the value we found at that address into the register, given to us in the first parameter
+}//{"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}, 11, 25,
+//     "store index", 1, 1},
+
+                
+
+void		apply_lld(t_data *data, t_list *elem) //addresses not relative to IDX
+{
+      if (param_type[0] == DIR_CODE)
+        elem->reg_number[params[1]] = params[0];// param[0] is the first parameter
+    else if (param_type[0] == IND_CODE)
+        elem->reg_number[params[1]] = get_ind_value(params[0]);
+    else if (param_types[0] == REG_CODE)
+        elem->reg_number[params[1]] = elem->reg_number[params[0]];
+}
+
+void		apply_lldi(t_data *data, t_list *elem) //addresses not relative to IDX
+{
+
+    int value_one;
+    int value_two;
+    int store; //value to store into the given register
+
+    if (param_types[0] == DIR_CODE)
+        value_one = params[0];
+    else if (param_types[0] == IND_CODE)
+        value_one = get_ind_value_idxd(params[0]);
+    else if (param_types[0] == REG_CODE)
+        value_one = elem->reg_number[params[0]];
+    if (param_types[1] == DIR_CODE)
+        value_two = params[1]
+    else if (param_types[1] == REG_CODE)
+        value_two = elem->reg_number[params[1]];
+    store = get_ind_value_idxd(value_one + value_two);
+    elem->reg_number[params[2]] = store;
+    //figure out carry modification
+}
+
 //void		apply_lfork(t_data *data, t_list *elem) //addresses not relative to IDX!
 //{
-//
+//  //Again... figure out how to split a process
 //}
 //
 //void		apply_aff(t_data *data, t_list *elem)
 //{
-//
+// //IDK what the fuck this means
 //}
 //
+
+//void      apply_fork(t_data *data, t_list *elem)
+//{
+// //figure out how to split a process
+//}
+//
+
 
 //deteermining the paramater types below
 char *conv_dec_to_bin(int n)
@@ -359,16 +400,24 @@ int     *det_types(unsigned int parameter_types)
 
 //determining the parameter types above 
 
-
-int         get_ind_value(t_data *data, t_list *elem, int val_pos)
+int         get_ind_value_idxd(t_data *data, t_list *elem, int add_to_pc)
 {
     int ind_value;
-    int add_by; //this plus the pc is the address of the value looked for
 
-    add_by = data->map[(elem->pc + val_pos) % MEM_SIZE];
-    ind_value = (elem->pc + (add_by % IDX_MOD)) % MEM_SIZE; //the address of the indirect value
+    ind_value = data->map[(elem->pc + (add_to_pc % IDX_MOD)) % MEM_SIZE]; //the value of the indirect value at its address
     return (ind_value);
 }// function to get the indirect value's ADDRESSS into the params array
+
+
+int         get_ind_value(t_data *data, t_list *elem, int add_to_pc)
+{
+    int ind_value;
+    int ind_address;
+
+    ind_address = elem->pc + add_to_pc; //adding the value found in the parameter with the pc, giving us the address of the indirect value
+    ind_value = data->map[ind_address % MEM_SIZE]; //getting the value from the memory
+    return (ind_value);
+}
 
 int         *get_params(int *par_types, t_data *data, t_list *elem) //if the ocp is there
 {
@@ -380,13 +429,12 @@ int         *get_params(int *par_types, t_data *data, t_list *elem) //if the ocp
         return (NULL);
     while (par_types[i] != 0) //this loop check the param types and fills the param array wtih the corresponding values in order
     {
-        if (par_types[i] == REG_CODE)
-            params[i] = data->map[((elem->pc + ((i + 2) % IDX_MOD))) % MEM_SIZE]]; //getting the register number
+        if (par_types[i] == REG_CODE || par_types[i] == IND_CODE) //this gets either the value the we need to jump to relative to the pc for the indirect, or the reg number
+            params[i] = data->map[(elem->pc + (i + 2)) % MEM_SIZE]; //getting the register number
         else if (par_types[i] == DIR_CODE)
-            params[i] = ((elem->pc + ((i + 2) % IDX_MOD))) % MEM_SIZE];
-        else if (par_types[i] == IND_CODE)
-            params[i] = get_ind_value(data, elem, (i + 2) % IDX_MOD);
-        i++;                                             //I'm not entirely sure about this IDX above
+            params[i] = //learn how to get parameters from several bytes
+
+        i++;
     }
     return (params);
 }// this function is self-explanatory, we're getting the parameters guys..
@@ -404,23 +452,16 @@ void        instruction_exec(t_data *data, t_list *elem)
     if (opc == 0x0C || opc == 0x09 || opc == 0x01)
     {
         instr_no_ocp(data, elem);
-        //free all the shit first dude
-        return ;
+        return ; //free all the shit first dude
     }
     instr_w_ocp(data, elem, params, param_types);        
 }
 
 
+ //if the last flag in the instructions is at 0 that means we need to get the direct parameter out of 4 bytes.
+
 
 //essential question: During the extraciton of the paramters do we also put the addresses realtive to IDX?
 //example if there is a direct value to be taken out of the first parameter, do we take pc + 2 or pc + (2 % IDX_MOD) ??
-
-
-
-
-
-
-
-
 
 
