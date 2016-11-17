2091d04272c5aa2732136b5f87eee27eacea1123
Jonathan LE QUERE <jle-quer@e3r9p11.42.fr>
2016-11-17 13:49:10 +0100

    Debug Instructions in progress, Set Color on writing process to do.

--- instructions/lld_lldi.c
+++ instructions/lld_lldi.c
@@ -6,7 +6,7 @@
 /*   By: tbui <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/20 18:12:49 by tbui              #+#    #+#             */
-/*   Updated: 2016/11/07 18:14:52 by lbaudran         ###   ########.fr       */
+/*   Updated: 2016/11/17 13:49:05 by jle-quer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 
 void		apply_lld(t_data *data, t_list *elem, unsigned int *param_types, unsigned int *params) //addresses not relative to IDX
 {
-      if (param_types[0] == DIR_CODE)
-        elem->reg_number[params[1]] = params[0];// param[0] is the first parameter
-    else if (param_types[0] == IND_CODE)
-        elem->reg_number[params[1]] = get_ind_value(data, elem, params[0]);
-    else if (param_types[0] == REG_CODE)
-        elem->reg_number[params[1]] = elem->reg_number[params[0]];
+	if (param_types[0] == DIR_CODE)
+		elem->reg_number[params[1]] = params[0];// param[0] is the first parameter
+	else if (param_types[0] == IND_CODE)
+		elem->reg_number[params[1]] = get_ind_value(data, elem, params[0]);
+	else if (param_types[0] == REG_CODE)
+		elem->reg_number[params[1]] = elem->reg_number[params[0]];
 	if (!elem->carry)
 		elem->carry = 1;
 	else if (elem->carry)
@@ -30,25 +30,27 @@ void		apply_lld(t_data *data, t_list *elem, unsigned int *param_types, unsigned 
 void		apply_lldi(t_data *data, t_list *elem, unsigned int *param_types, unsigned int *params) //addresses not relative to IDX
 {
 
-    unsigned int value_one;
-    unsigned int value_two;
-    unsigned int store; //value to store into the given register
+	unsigned int	value_one;
+	unsigned int	value_two;
+	unsigned int	store; //value to store into the given register
 
-    if (param_types[0] == DIR_CODE)
-        value_one = params[0];
-    else if (param_types[0] == IND_CODE)
-        value_one = get_ind_value(data, elem, params[0]);
-    else if (param_types[0] == REG_CODE)
-        value_one = elem->reg_number[params[0]];
-    if (param_types[1] == DIR_CODE)
-        value_two = params[1];
-    else if (param_types[1] == REG_CODE)
-        value_two = elem->reg_number[params[1]];
-    store = get_ind_value(data, elem, (value_one + value_two));
-    elem->reg_number[params[2]] = store;
+	store = 0;
+	value_one = 0;
+	value_two = 0;
+	if (param_types[0] == DIR_CODE)
+		value_one = params[0];
+	else if (param_types[0] == IND_CODE)
+		value_one = get_ind_value(data, elem, params[0]);
+	else if (param_types[0] == REG_CODE)
+		value_one = elem->reg_number[params[0]];
+	if (param_types[1] == DIR_CODE)
+		value_two = params[1];
+	else if (param_types[1] == REG_CODE)
+		value_two = elem->reg_number[params[1]];
+	store = get_ind_value(data, elem, (value_one + value_two));
+	elem->reg_number[params[2]] = store;
 	if (!elem->carry)
 		elem->carry = 1;
 	else if (elem->carry)
 		elem->carry = 0;
-    //figure out carry modification
 }
